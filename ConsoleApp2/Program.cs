ee62248e239558e68688ad728dd84e961ab299e5
bburakbbasol <bburakbbasol@gmail.com>
2024-12-20 12:03:56 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Collections.Concurrent;
+Console.Write("Lütfen Sayı Giriniz:");
+string strnum = Console.ReadLine();
+
+int num = Convert.ToInt32(strnum);
+
+if ( num > 10)
+{
+	if(num%2==0)
+	{
+		Console.WriteLine("Girelen Sayı Çiftdir");
+	}
+	else
+	{
+		Console.WriteLine("Girilen Sayı Tekdir");
+	}
+}
+else 
+{
+
+	Console.WriteLine("Girilen Sayı 10 dan küçüktür");
+}
